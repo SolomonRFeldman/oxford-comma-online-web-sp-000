f0283da10096c6ad009aec75414fdeebe97a22bf
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-17 17:27:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,8 @@
 def oxford_comma(series)
-  # if series.size == 1
-  #   series[0]
   if series.size < 3
     series.join(" and ")
   else
     series[-1] = "and #{series[-1]}"
     series.join(", ")
-    
   end
 end
